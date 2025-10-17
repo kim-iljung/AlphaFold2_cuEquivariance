b658153b0678f21cc41a4de17396cef013837f72
Iljung Kim <60723084+kim-iljung@users.noreply.github.com>
2025-10-17 21:19:15 +0900

    Fix cuEquiv column attention bias handling

--- module/chunked_attention.py
+++ module/chunked_attention.py
@@ -1,6 +1,5 @@
 import torch
 from typing import List
-from torch.utils.checkpoint import checkpoint
 
 def permute_final_dims(tensor: torch.Tensor, inds: List[int]):
     zero_index = -1 * len(inds)
@@ -25,18 +24,8 @@ def _attention(query: torch.Tensor, key: torch.Tensor, value: torch.Tensor, bias
     return a
 
 def _attention_chunked_trainable(
-    query, key, value, biases, chunk_size, chunk_dim, checkpoint_flag, 
+    query, key, value, biases, chunk_size, chunk_dim,
 ):
-    if checkpoint_flag and len(biases) > 2:
-        raise ValueError(
-            "Checkpointed version permits only permits two bias terms"
-        )
-
-    def _checkpointable_attention(q, k, v, b1, b2):
-        bs = [b for b in [b1, b2] if b is not None]
-        a = _attention(q, k, v, bs)
-        return a
-
     o_chunks = []
     count = query.shape[chunk_dim]
     for start in range(0, count, chunk_size):
@@ -54,21 +43,11 @@ def _attention_chunked_trainable(
             )
             return b[tuple(idx)]
 
-        if checkpoint_flag:
-            bias_1_chunk, bias_2_chunk = [
-                _slice_bias(b) if b is not None else None
-                for b in (biases + [None, None])[:2]
-            ]
+        bias_chunks = [
+            _slice_bias(b) for b in biases
+        ]
 
-            o_chunk = checkpoint(_checkpointable_attention,
-                q_chunk, k_chunk, v_chunk, bias_1_chunk, bias_2_chunk
-            )
-        else:
-            bias_chunks = [
-                _slice_bias(b) for b in biases
-            ]
-
-            o_chunk = _attention(q_chunk, k_chunk, v_chunk, bias_chunks)
+        o_chunk = _attention(q_chunk, k_chunk, v_chunk, bias_chunks)
             
         o_chunk = o_chunk.transpose(-2, -3)
         o_chunks.append(o_chunk)
